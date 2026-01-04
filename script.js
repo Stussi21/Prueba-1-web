5fb6a5c8f6a608599145a5904d16eb33ebd75568
Stussi21 <m.tapia.valenzuela21@gmail.com>
2026-01-04 02:39:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const cerrarMenuBtn = document.getElementById('cerrarMenuBtn');
     const menuSection = document.getElementById('menu-section');
 
-    // Al hacer clic en el botón de ver menú
     verMenuBtn.addEventListener('click', () => {
         menuSection.classList.remove('hidden');
-        menuSection.style.display = 'block'; // Asegura que se vea
     });
 
-    // Al hacer clic en el botón de cerrar
     cerrarMenuBtn.addEventListener('click', () => {
         menuSection.classList.add('hidden');
-        menuSection.style.display = 'none';
     });
 });
